bea6c33b02475d7d1a5ce611c3dc4a8bc4bba2b9
Garrett <garrettgsb@gmail.com>
2016-06-11 14:53:42 -0700

    Completed first draft index page

---
+++ public/javascript/garrettscript/bubble_generator.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  var $aquarium = $(".aquarium");
+  setInterval(function(){
+    $aquarium.append($(bubbleGenerate()));
+    $aquarium.append($(bubbleGenerate()));
+    $aquarium.append($(bubbleGenerate()));
+    $aquarium.append($(bubbleGenerate()));
+    $aquarium.append($(bubbleGenerate()));
+    $aquarium.append($(bubbleGenerate()));
+    $aquarium.append($(bubbleGenerate()));
+    $aquarium.append($(bubbleGenerate()));
+    $aquarium.append($(bubbleGenerate()));
+    $aquarium.append($(bubbleGenerate()));
+    $aquarium.append($(bubbleGenerate()));
+    $aquarium.append($(bubbleGenerate()));
+
+
+    $('.bubble').each(function(i,e) {$(e).animate({marginTop: -40}, $(e).data("riseSpeed"), function(){
+      if ($(e).position()['top'] < 1) { $(e).remove();}
+    });
+    })
+
+
+  }, 4000);
+
+  var $bottom = $(".footer");
+  var $bubble = "<div class='bubble'></div>";
+  $bottom.before($bubble);
+
+  var nextId = 0;
+
+  function bubbleGenerate() {
+      var diameter = Math.random() * (40-5) + 5;
+      var fillColor = "rgba(40,80," + Math.round(240-(diameter*3)) + "," + (5 / diameter);
+      bubble = document.createElement("div");
+      $(bubble).attr('class', 'bubble');
+
+      //Random diameter between 5 and 30 px
+      //Rise speed (and soon, color) is based on diameter
+
+      var horiz = Math.random() * (($(document).width()) - (diameter*2)) + diameter;
+      $(bubble).data("riseSpeed", (diameter * 2000));
+      $(bubble).css({height: diameter, width: diameter, marginTop: $(window).height() + 50, marginLeft: horiz, borderColor: fillColor});
+      return bubble;
+  }
+
+  });
