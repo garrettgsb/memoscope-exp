ca9cf829b000d889614b499e43c91c6ac7065020
Jordan <jordan.ratanadawong@gmail.com>
2016-06-12 08:04:11 +0000

    Made navigation awesome, and seperated card and cards views.

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,5 +2,19 @@ $(function(){
 	$(".card-header-icon").on('click', function(){
 		$(this).parent().parent().hide();
 	});
+	$(window).on('scroll', function(){
+		if ($(this).scrollTop() > $("#section1").offset().top){
+			$(".nav-circle").addClass("unselected");
+			$('.section1').removeClass("unselected");
+		}
+		if ($(this).scrollTop() > $("#section2").offset().top){
+			$(".nav-circle").addClass("unselected");
+			$('.section2').removeClass("unselected");
+		}
+		if ($(this).scrollTop() > $("#section3").offset().top){
+			$(".nav-circle").addClass("unselected");
+			$('.section3').removeClass("unselected");
+		}
 
+	});
 });
