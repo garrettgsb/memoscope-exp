66a4d454d9de38c15b1930120fa6510e4465465f
Jordan <jordan.ratanadawong@gmail.com>
2016-06-12 08:14:48 +0000

    Added smooth scrolling and better anchoring on click

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -3,18 +3,30 @@ $(function(){
 		$(this).parent().parent().hide();
 	});
 	$(window).on('scroll', function(){
-		if ($(this).scrollTop() > $("#section1").offset().top){
+		if ($(this).scrollTop() > $("#section1").offset().top - 50){
 			$(".nav-circle").addClass("unselected");
 			$('.section1').removeClass("unselected");
 		}
-		if ($(this).scrollTop() > $("#section2").offset().top){
+		if ($(this).scrollTop() > $("#section2").offset().top - 50){
 			$(".nav-circle").addClass("unselected");
 			$('.section2').removeClass("unselected");
 		}
-		if ($(this).scrollTop() > $("#section3").offset().top){
+		if ($(this).scrollTop() > $("#section3").offset().top - 50){
 			$(".nav-circle").addClass("unselected");
 			$('.section3').removeClass("unselected");
 		}
 
 	});
+	  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
