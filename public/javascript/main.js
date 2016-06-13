12d7b49ba9d6430c483d6a9b43ad88f32d2d5919
Jordan <jordan.ratanadawong@gmail.com>
2016-06-13 02:11:27 +0000

    Added carousel to dashboard

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,7 +1,26 @@
 $(function(){
-	$(".card-header-icon").on('click', function(){
-		$(this).parent().parent().remove();
+	$(".columns .card-header-icon").on('click', function(){
+		var thisCard = $(this).parent().parent().parent();
+		deleteCard(thisCard);
 	});
+
+	function deleteCard(card){
+		$(card).delay(100).fadeOut(1000);
+    $(card).animate({
+      "opacity" : "0",
+      },{
+      "complete" : function() {
+        $(card).remove();
+      }
+  	});
+	}
+
+	$('.slider-container .card-header-icon').on('click', function() {
+		var slideIndex = $(this).parent().parent().parent().data("slick-index");
+		console.log(slideIndex);
+	  $('.slider-container').slick('slickRemove', slideIndex);
+	});
+
 	$(window).on('scroll', function(){
 		if ($(this).scrollTop() > $("#section1").offset().top - 60){
 			$(".nav-circle").addClass("unselected");
@@ -37,5 +56,34 @@ $(function(){
 	});
 	$('.modal-background').on('click', function(){
 		$('.modal').removeClass('is-active');
+	});
+
+  $('.slider-container').slick({
+	  centerMode: true,
+	  centerPadding: '60px',
+	  slidesToShow: 3,
+	  responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: true,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+	$('.slick-prev').on('click', function(){
+		$('.slider-container').slickPrev();
 	});
 });
