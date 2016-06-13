5d2db756f6d2fdc93b00d1399c0f411e8c9a80ba
Garrett <garrettgsb@gmail.com>
2016-06-12 19:23:37 -0700

    Can now trigger edit by clicking anywhere on card, or edit button. Added render/edit/highlight buttons... Highlight doesn't do anything.

--- public/javascript/garrettscript/card-new.js
+++ public/javascript/garrettscript/card-new.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
   console.log(`Loaded card-new.js`);
 
 var editBox = "<textarea class='edit-mode' rows='6' cols='80'>";
-var editModeTag = `<span class="tag is-danger edit-mode"><i class="fa fa-edit"></i>&nbsp Edit Mode</span><p class="edit-mode"><small>Click outside of text box to stop editing</small></p>`;
-var renderModeTag = `<span class="tag is-info render-mode"><i class="fa fa-eye"></i>&nbsp Render Mode</span><p class="render-mode"><small>Click text to edit</small></p>`;
 
 function editMode(text) {
   console.log(text)
-  $(".card").prepend(editModeTag);
+  $(".edit-light").addClass('is-danger');
+  $(".render-light").removeClass('is-primary');
+  $(".highlight-light").removeClass('is-primary');
   $("h5").after(editBox);
   $("textarea").text(text);
   $(".render-mode").css('display', 'none');
@@ -16,21 +16,20 @@ function editMode(text) {
 
   function renderMode(text){
     console.log(text);
-    $("span.render-mode").css('display', 'inline');
-    $("p.render-mode").css('display', 'block');
+    $(".render-light").addClass('is-primary');
+    $(".edit-light").removeClass('is-danger');
+    $(".highlight-light").removeClass('is-primary');
     $("h5").text(text).css('display', 'inline-block');
     $(".edit-mode").remove();
   }
 
-$("h5").on('click', function(){
+$(".editable").on('click', function(){
   console.log("clicked!")
-  editMode($(this).text());
+  editMode($("h5").text());
   return $("textarea").on('blur', function(){
     console.log("blurred!");
     renderMode($("textarea").val());
   });
 })
-
-$(".content.editable").prepend(renderModeTag);
 
 });
