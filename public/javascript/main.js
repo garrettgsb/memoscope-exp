f324abc570b91ee9b9d50290671daf5503a7f546
Jordan <jordan.ratanadawong@gmail.com>
2016-06-14 17:12:43 +0000

    Updated the navbar to have menu items

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -78,6 +78,61 @@ $(function(){
       }
     }
   });
+	// Navigation stuff
+	$(".user-nav").hover(function(){
+		$(".user-menu").css({
+			"top": $(".nav").offset().top + $(".nav").outerHeight() -2,
+			"right": 0,
+			"display": "block"
+		})
+	}, function(){
+			$(".user-menu").css({"display": "none"});
+	});
+
+	$(".user-menu").hover(function(){
+		$(".user-menu").css({"display": "block"});
+	}, function(){
+		$(".user-menu").css({"display": "none"});
+	});
+
+
+	$(".deck-nav").hover(function(){
+		$(".deck-menu").css({
+			"top": $(".nav").offset().top + $(".nav").outerHeight() -2,
+			"left": $(".deck-nav").offset().left,
+			"display": "block"
+		})
+	}, function(){
+			$(".deck-menu").css({"display": "none"});
+	});
+
+	$(".deck-menu").hover(function(){
+		$(".deck-menu").css({"display": "block"});
+	}, function(){
+		$(".deck-menu").css({"display": "none"});
+	});
+
+
+	$(".card-nav").hover(function(){
+		$(".card-menu").css({
+			"top": $(".nav").offset().top + $(".nav").outerHeight() -2,
+			"left": $(".card-nav").offset().left,
+			"display": "block"
+		})
+	}, function(){
+			$(".card-menu").css({"display": "none"});
+	});
+
+	$(".card-menu").hover(function(){
+		$(".card-menu").css({"display": "block"});
+	}, function(){
+		$(".card-menu").css({"display": "none"});
+	});
+
+	//End Navigation
+
+
+
 	$('.card').on('click', function(){
 		$('.modal').toggleClass('is-active');
 	});
