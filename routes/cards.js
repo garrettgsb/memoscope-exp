d553e24a25deb165d28ebdf88250f7093f74770b
“soheilhm” <soheil.hm@gmail.com>
2016-06-14 14:44:26 -0700

    rearrnaged routes in the conslidated views

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const router = express.Router();
+// sequelize initialization
+const Sequelize = require('sequelize');
+var pg = require('pg').native;
+var config = require('../db_config');
+var db = require('../server/models');
+var User = require('../server/models').User;
+var Card = require('../server/models').Card;
+var Deck = require('../server/models').Deck;
+var sequelize = new Sequelize(config.database, config.username, config.password, {
+      host: config.host,
+      port: config.port,
+      dialect: config.dialect,
+      native: config.native
+    });
 
+var user_name;
+
+User.findById(1).then(
+  function(user){
+    user_name = user.username;
+    console.log('Current user is: ' + user_name);
+});
 /* GET home page. */
 
 
