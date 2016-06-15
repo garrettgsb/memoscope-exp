35e45646a6569235498427abc6edaa54f2953ebd
“soheilhm” <soheil.hm@gmail.com>
2016-06-15 16:19:41 -0700

    SQL database initialized

--- db_config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  database: 'memoscope_database', 
-  username: 'development', 
-  password: 'development',
-  host: 'localhost',
-  port: 5432,
-  dialect: 'postgres',
-  native: true
-};
