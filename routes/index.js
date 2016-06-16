87e47361f962dbdababca16b2ae5759b0b378341
Jordan Ratanadawong <jordan.ratanadawong@gmail.com>
2016-06-15 19:47:07 -0700

    Setting up routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // database initialization
 var pg = require('pg').native;
-const connectionString = "postgres://development:development@localhost/memoscope_database";
+const connectionString = "postgres://development:development@localhost/memoscope";
 
 function queryParams(sql, params, cb) {
   pg.connect(connectionString, function(err, db, done) {
