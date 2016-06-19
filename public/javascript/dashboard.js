0a9d71dfeb67e099e3e9fef6a12b143e421c859d
“soheilhm” <soheil.hm@gmail.com>
2016-06-19 01:03:13 -0700

    Orbis trigonometry improved, shadows and background added

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
       var sun = {
         x: xOffset,
         y: yOffset,
-        r: 30
+        r: 20
       };
 
       var cards = [{
@@ -64,19 +64,19 @@ $(document).ready(function(){
       }, {
         orbit: 2,
         notifiedAt: 1466197800701,
-        r: 12
+        r: 14
       }, {
         orbit: 3,
         notifiedAt: 1466197442401,
-        r: 10
+        r: 13
       }, {
         orbit: 4,
         notifiedAt: 1466197898701,
-        r: 8
+        r: 12
       }, {
         orbit: 5,
         notifiedAt: 1466197897701,
-        r: 6
+        r: 11
       }];
       function moveCard(card) {
         var orbit = orbits[card.orbit];
@@ -87,8 +87,8 @@ $(document).ready(function(){
         var percent = delta / total;
         var angle = 360 * percent;
         angle = angle * (Math.PI / 180);
-        var x = (2.5 * orbit.radius * Math.cos(angle))/Math.sqrt((6.25 * Math.pow(Math.sin(angle),2)) + Math.pow(Math.cos(angle),2));
-        var y = (2.5 * orbit.radius * Math.sin(angle))/Math.sqrt((6.25 * Math.pow(Math.sin(angle),2)) + Math.pow(Math.cos(angle),2));
+        var x = (2 * orbit.radius * Math.cos(angle))/Math.sqrt((4 * Math.pow(Math.sin(angle),2)) + Math.pow(Math.cos(angle),2));
+        var y = (2 * orbit.radius * Math.sin(angle))/Math.sqrt((4 * Math.pow(Math.sin(angle),2)) + Math.pow(Math.cos(angle),2));
         card.x = x + xOffset;
         card.y = y + yOffset;
       }
@@ -101,36 +101,46 @@ $(document).ready(function(){
 
       function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.font = "3.125em serif";
+        ctx.fillText("Memoscope", xOffset - (canvas.width/9),yOffset - (canvas.height/2.5));
         for (var orbit in orbits) {      
           var b = orbits[orbit].radius;
-          var a = 2.5 * b;
+          var a = 2 * b;
           ctx.beginPath();
           ctx.ellipse(xOffset, yOffset, a, b , 0 , 0, 2 * Math.PI);
-          ctx.stroke();
           var time = orbits[orbit].time / 1000;
           var orbit_time;
           switch(time) {
               case 10:
                   orbit_time = "10 sec";
+                  ctx.strokeStyle = '#1A0D9E';
                   break;
               case 30:
                   orbit_time = "30 sec";
+                  ctx.strokeStyle = '#3B0D9E';
                   break;
               case 120:
                   orbit_time = "2 min";
+                  ctx.strokeStyle = '#4F0D9E';
                   break;
               case 600:
                   orbit_time = "10 min";
+                  ctx.strokeStyle = '#5E0D9E';
                   break;
               default:
                   orbit_time = "1 hour";
+                  ctx.strokeStyle = '#700D9E';
           }
-          ctx.font = "20px serif";
-          ctx.fillText(orbit_time, xOffset - ( a / 2), (yOffset +  b ));
+          ctx.stroke();
+          ctx.font = "1.5em serif";
+          ctx.fillText(orbit_time, xOffset + (a / 3), (yOffset +  b ));
+          ctx.font = "1.5em serif";
+          ctx.fillText(orbit_time, xOffset - (a / 3), (yOffset -  b ));
         }
         cards.forEach(function (card) {
           ctx.beginPath();
-          ctx.fillStyle = "rgb(174,52,199)";
+          ctx.fillStyle = "rgb(69,113,181)";
           ctx.shadowColor = "black";           
           ctx.arc(card.x, card.y, card.r, 0, 2 * Math.PI, true);
           ctx.fill();
@@ -145,13 +155,16 @@ $(document).ready(function(){
         });
         ctx.beginPath();
         ctx.fillStyle = "rgb(216,232,70)";
-        ctx.shadowBlur = 5;
+        ctx.shadowBlur = 20;
         ctx.arc(sun.x, sun.y, sun.r, 0, 2 * Math.PI, true);
         ctx.fill();
         ctx.beginPath();
         ctx.fillStyle = "rgb(255,255,255)";
         ctx.shadowColor = "white";
-        ctx.arc(sun.x, sun.y, (sun.r / 1.5), 0, 2 * Math.PI, true);
+        ctx.shadowOffsetX = (sun.r / 3) ;
+        ctx.shadowOffsetY = (sun.r / 3) ;
+        ctx.shadowBlur = 20;              
+        ctx.arc(sun.x - (sun.r / 2.5), sun.y - (sun.r / 2.5) , (sun.r/3), 0, 2 * Math.PI, true);
         ctx.fill();
       }
 
