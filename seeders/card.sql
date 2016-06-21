710b01c07c7e24ecf1b5188a08e5363bf4d27f95
Garrett <garrettgsb@gmail.com>
2016-06-20 17:14:26 -0700

    And then everything changed.

--- seeders/card.sql
+++ seeders/card.sql
@@ -1,35 +1,35 @@
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (1, '“You talk when you cease to be at peace with your thoughts.” ― Kahlil Gibran',1,null, current_timestamp, current_timestamp);
+VALUES (1, '“You <span class="chunk highlighter-red">talk</span> when you <span class="chunk highlighter-yellow">cease to be at peace with your thoughts</span>.” ― Kahlil Gibran',1,null, current_timestamp, current_timestamp);
 
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (1, '“Poets have been mysteriously silent on the subject of cheese.” ― G.K. Chesterton',2,null, current_timestamp, current_timestamp);
+VALUES (1, '“Poets have been <span class="chunk highlighter-red">mysteriously silent</span> on the subject of <span class="chunk highlighter-blue">cheese.”</span> ― <span class="chunk highlighter-green">G.K. Chesterton</span>',2,null, current_timestamp, current_timestamp);
 
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (2, '"There is no such thing as a free lunch."',4,null, current_timestamp, current_timestamp);
+VALUES (2, '"<span class="chunk highlighter-red">There is no such thing</span> as a <span class="chunk highlighter-blue">free lunch</span>."',4,null, current_timestamp, current_timestamp);
 
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (2, '"God helps those who help themselves."',3,null, current_timestamp, current_timestamp);
+VALUES (2, '"<span class="chunk highlighter-red">God</span> helps those who <span class="chunk highlighter-blue">help themselves</span>."',3,null, current_timestamp, current_timestamp);
 
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (3, '"A 19th century horse named ‘Old Billy’ is said to have lived 62 years."',3,null, current_timestamp, current_timestamp);
+VALUES (3, '"A 19th century horse named <span class="chunk highlighter-red">‘Old Billy’</span> is said to have lived <span class="chunk highlighter-green">62 years</span>."',3,null, current_timestamp, current_timestamp);
 
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (3, '"A male horse is called a stallion and a young male horse is called a colt."',4,null, current_timestamp, current_timestamp);
+VALUES (3, '"A male horse is called a <span class="chunk highlighter-red">stallion</span> and a young male horse is called a <span class="chunk highlighter-blue">colt</span>."',4,null, current_timestamp, current_timestamp);
 
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (4, '"The color of the wine tells you about its geographical origin"',2,null, current_timestamp, current_timestamp);
+VALUES (4, '"The <span class="chunk highlighter-yellow">color</span> of the wine tells you about its <span class="chunk highlighter-blue">geographical origin</span>"',2,null, current_timestamp, current_timestamp);
 
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (4, '"Wine was discovered about 6,000 years ago in the Middle East"',1,null, current_timestamp, current_timestamp);
+VALUES (4, '"Wine was discovered about <span class="chunk highlighter-red">6,000</span> years ago in <span class="chunk highlighter-blue">the Middle East</span>"',1,null, current_timestamp, current_timestamp);
 
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (5, '"The skin loses about 30,000 to 40,000 dead skin cells from the surface almost every minute"',4,null, current_timestamp, current_timestamp);
+VALUES (5, '"The skin loses about <span class="chunk highlighter-red">30,000 to 40,000</span> dead skin cells from the surface <span class="chunk highlighter-blue">almost every minute</span>"',4,null, current_timestamp, current_timestamp);
 
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (5, '"Each 5 square cm of skin may have up to 600 sweat glands."',2,null, current_timestamp, current_timestamp);
+VALUES (5, '"Each <span class="chunk highlighter-red">5 square cm</span> of skin may have up to <span class="chunk highlighter-yellow">600 sweat</span> glands."',2,null, current_timestamp, current_timestamp);
 
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (6, '"Humans and cabbage share about 40-50% common DNA."',2,null, current_timestamp, current_timestamp);
+VALUES (6, '"Humans and <span class="chunk highlighter-green">cabbage</span> share about <span class="chunk highlighter-red">40-50%</span> common DNA."',2,null, current_timestamp, current_timestamp);
 
 INSERT INTO "cards" (deck_id, content_html, orbit, notified_at, created_at, modified_at)
-VALUES (6, '"You have 98% of your DNA in common with a chimpanzee."',8,null, current_timestamp, current_timestamp);
+VALUES (6, '"You have <span class="chunk highlighter-red">98%</span> of your DNA in common with <span class="chunk highlighter-green">a chimpanzee</span>."',8,null, current_timestamp, current_timestamp);
